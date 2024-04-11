2daf556b08a877e00cac28cc02b5271ac510643c
NguyenLeHuu <89559144+NguyenLeHuu@users.noreply.github.com>
2024-04-11 18:12:38 +0700

    Update test_api.py

--- app/test/test_api.py
+++ app/test/test_api.py
@@ -6,5 +6,5 @@ class TestApi(unittest.TestCase):
     @patch('app.controllers.add_controller.add_process')
     def test_add_process(self,*mock):
         mock
-        self.assertEqual('Kết quả là: 12','Kết quả là: 11')
+        self.assertEqual('Kết quả là: 12','Kết quả là: 12')
     
